504f24a2a007b4405e40496b5154b3aba4230c53
jvk <jvkkoppineedi@gmail.com>
2024-04-06 18:30:00 +0530

    added inverse function to HillCipher.java

--- HillCipher.java
+++ HillCipher.java
@@ -1,11 +1,9 @@
-import java.io.*;
 import java.util.*;
-
 
 public class HillCipher {
     static float[][] decrypt = new float[3][1];
-    static float[][] a = {{1,2,3}, {0, 1, 4}, {5, 6, 0}};
-    static float[][] b = {{-24, 18, 5}, {20, -15, -4}, {-5, 4, 1}};
+    static float[][] a = new float[3][3];
+    static float[][] b = new float[3][3];
     static float[][] mes = new float[3][1];
     static float[][] res = new float[3][1];
     static Scanner sc = new Scanner(System.in);
@@ -21,6 +19,7 @@ public class HillCipher {
         for (int i = 0; i < 3; i++) {
             System.out.print((char) (res[i][0] % 26 + 97));
         }
+        inverse();
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 1; j++)
                 for (int k = 0; k < 3; k++) {
@@ -38,5 +37,43 @@ public class HillCipher {
         String msg = sc.next();
         for (int i = 0; i < 3; i++)
             mes[i][0] = msg.charAt(i) - 97;
+        System.err.println("Enter key matrix of 3x3 matrix: ");
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                a[i][j] = sc.nextFloat();
     }
+
+    public static void inverse() {
+
+        float det = a[0][0] * (a[1][1] * a[2][2] - a[1][2] * a[2][1])
+                - a[0][1] * (a[1][0] * a[2][2] - a[1][2] * a[2][0])
+                + a[0][2] * (a[1][0] * a[2][1] - a[1][1] * a[2][0]);
+
+        if (det == 0) {
+            System.out.println("Matrix is singular, cannot find inverse.");
+        }
+
+        float invDet = 1 / det;
+
+        b[0][0] = (a[1][1] * a[2][2] - a[1][2] * a[2][1]);
+        b[0][1] = (a[0][2] * a[2][1] - a[0][1] * a[2][2]);
+        b[0][2] = (a[0][1] * a[1][2] - a[0][2] * a[1][1]);
+        b[1][0] = (a[1][2] * a[2][0] - a[1][0] * a[2][2]);
+        b[1][1] = (a[0][0] * a[2][2] - a[0][2] * a[2][0]);
+        b[1][2] = (a[0][2] * a[1][0] - a[0][0] * a[1][2]);
+        b[2][0] = (a[1][0] * a[2][1] - a[1][1] * a[2][0]);
+        b[2][1] = (a[0][1] * a[2][0] - a[0][0] * a[2][1]);
+        b[2][2] = (a[0][0] * a[1][1] - a[0][1] * a[1][0]);
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+                b[i][j] = b[i][j] * invDet;
+                if (b[i][j] < 0)
+                    b[i][j] = (b[i][j] % 26) + 26;
+                else
+                    b[i][j] = b[i][j] % 26;
+            }
+
+    }
+
 }
